4791ec98c4e16e2dd4350f372fb3a20c23d21cf3
mattia-dilu <diluca.mattia02@gmail.com>
2025-11-20 19:44:19 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1>Vue Dashboard Example</h1>
+    <ChartExample />
+  </div>
+</template>
+
+<script setup>
+import ChartExample from './components/ChartExample.vue'
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background: #f3f3f3;
+}
+.container {
+  padding: 20px;
+}
+</style>
---
+++ src/components/ChartExample.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-chart :option="chartOptions" style="width: 100%; height: 400px;" />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { use } from "echarts/core"
+import { CanvasRenderer } from "echarts/renderers"
+import { BarChart } from "echarts/charts"
+import { GridComponent, TooltipComponent } from "echarts/components"
+import VChart from "vue-echarts"
+
+// Registrazione componenti ECharts
+use([
+  CanvasRenderer,
+  BarChart,
+  GridComponent,
+  TooltipComponent
+])
+
+const chartOptions = ref({
+  tooltip: {},
+  xAxis: {
+    data: ["Lun", "Mar", "Mer", "Gio", "Ven"]
+  },
+  yAxis: {},
+  series: [
+    {
+      name: "Vendite",
+      type: "bar",
+      data: [5, 20, 36, 10, 28]
+    }
+  ]
+})
+</script>
