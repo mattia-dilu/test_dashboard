67cb54a0ae32eb6b5433f4ac0188e0614d3d8ec9
mattia-dilu <diluca.mattia02@gmail.com>
2025-11-21 22:25:49 +0100

    fix geojson

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,31 +1,36 @@
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import * as echarts from "echarts";
 import VChart from "vue-echarts";
 
-// Import del GeoJSON ISTAT Liguria
-import liguria from "../data/liguria_istat.geojson";
+const option = ref(null);
 
-// Registrazione mappa
-echarts.registerMap("liguria", liguria);
+onMounted(async () => {
+  // carica il geojson dalla cartella public
+  const liguria = await fetch("/test_dashboard/liguria_istat.geojson")
+    .then((r) => r.json());
 
-// Opzioni mappa
-const option = ref({
-  tooltip: {},
-  geo: {
-    map: "liguria",
-    roam: true,
-    itemStyle: {
-      areaColor: "#0b0f17",
-      borderColor: "#4fb4ff",
-      borderWidth: 2
-    },
-    emphasis: {
+  // registra la mappa
+  echarts.registerMap("liguria", liguria);
+
+  // opzioni mappa
+  option.value = {
+    tooltip: {},
+    geo: {
+      map: "liguria",
+      roam: true,
       itemStyle: {
-        areaColor: "#1a355e"
+        areaColor: "#0b0f17",
+        borderColor: "#4fb4ff",
+        borderWidth: 2
+      },
+      emphasis: {
+        itemStyle: {
+          areaColor: "#1a355e"
+        }
       }
     }
-  }
+  };
 });
 </script>
 
@@ -33,7 +38,6 @@ const option = ref({
   <div class="page">
     <h2 class="glow">Mappa Regione Liguria</h2>
 
-    <!-- IMPORTANTE: qui usiamo il componente registrato -->
-    <v-chart :option="option" style="width:100%; height:600px;" />
+    <v-chart v-if="option" :option="option" style="width:100%; height:600px;" />
   </div>
 </template>
