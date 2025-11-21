cde5ada5a0fed8c9b760434fa90734a3c031e6d9
mattia-dilu <diluca.mattia02@gmail.com>
2025-11-21 21:48:18 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  base: '/test_dashboard/'
+  base: '/test_dashboard/',
+  plugins: [vue()]
 })
+
